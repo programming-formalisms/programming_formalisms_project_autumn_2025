7c56f694009dc086d621d5889697468ba6e27959
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-10-17 11:47:31 +0200

    Cleanup

--- src/bacsim/andrei_utils.py
+++
@@ -1,7 +0,0 @@
-def is_zero(number):
-    """Testing if the number is zero."""
-    if not isinstance(number, int):
-        raise TypeError("'number' must be of type int.")
-    if number == 0:
-        return True
-    return False
--- src/learners/is_number.py
+++
@@ -1,5 +0,0 @@
-def is_number(x):
-    """
-    This function tests whether the input variable x is a number or not.
-    """
-    return isinstance(x, (int, float))
