140e7bae95bd29f8704373012ba6bd40b04df916
SLAment <SLAment@users.noreply.github.com>
2025-10-24 14:58:13 +0200

    New test using scaffolding

--- src/learners/lorena/lorena.py
+++ src/learners/lorena/lorena.py
@@ -8,3 +8,10 @@ def is_zero(input):
 assert is_zero.__doc__
 assert is_zero(0) == True
 assert is_zero(1) == False
+
+has_thrown = False
+try:
+    my_function("nonsense")
+except:
+    has_thrown = True
+assert has_thrown
