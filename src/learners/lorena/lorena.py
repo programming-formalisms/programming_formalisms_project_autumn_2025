789ba381145dbc01b6e8b4d19839ce166d2780eb
SLAment <SLAment@users.noreply.github.com>
2025-10-24 14:55:31 +0200

    Broke code again

--- src/learners/lorena/lorena.py
+++ src/learners/lorena/lorena.py
@@ -3,7 +3,8 @@
 
 def is_zero(input):
     """ Determins if the input is zero """
-    return 0
+    return True
 
 assert is_zero.__doc__
-assert is_zero(0) == 0
+assert is_zero(0) == True
+assert is_zero(1) == False
