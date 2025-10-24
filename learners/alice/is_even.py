74e3dac095f64a4ae01613e707f8a4c77dd839d9
alicefurl <alice.furlotti@geo.uu.se>
2025-10-24 15:22:43 +0200

    Added if-statement to return True for even numbers and false for odd numbers

--- learners/alice/is_even.py
+++ learners/alice/is_even.py
@@ -3,6 +3,10 @@
 def is_even(number):
     """Defines if input number is even"""
     # Here I have defined is_even
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 assert is_even.__doc__
- 
+assert is_even(2) == True
