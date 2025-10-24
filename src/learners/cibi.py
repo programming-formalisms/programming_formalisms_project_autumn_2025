a569ce0ed17a9abcfc38ca18d332078c36b78760
cibi-sundaram <cibisundaram.94@outlook.com>
2025-10-24 14:35:10 +0200

    Step 2: Added the parameters and if statement

--- src/learners/cibi.py
+++ src/learners/cibi.py
@@ -1,11 +1,14 @@
 
-def is_zero():
+def is_zero(input):
     """This is a function that checks if the input is equal to zero
     1. Returns True if input is zero
     2. Returns False if input is non-zero
     3. Gives error if input is not a number
     """
-
-    return
+    assert isinstance(input, (int,float))
+    if input == 0:
+        return True
+    else:
+        return False
 
 assert is_zero.__doc__
