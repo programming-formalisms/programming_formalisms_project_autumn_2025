837b7d600001e4ec0f2a96e8c593999fac080a21
afi3008 <annalenafischer3099@gmail.com>
2025-10-24 14:17:11 +0200

    make is_even  TDD

---
+++ learners/afi/afi.py
@@ -0,0 +1,16 @@
+def is_even(input):
+	"""
+	is the number even (True) or odd (False)
+	raise error if not a number
+	"""
+	if (input//2)==0:
+
+	return True
+
+
+
+assert is_even.__doc__
+assert is_even(2)
+assert not is_even(1)
+
+
