02c8d88513d6ab2d585fd7bb72d81d6278275c0b
afi3008 <annalenafischer3099@gmail.com>
2025-10-24 15:38:09 +0200

    raise error when not a number

--- learners/afi/is_even.py
+++ learners/afi/is_even.py
@@ -5,12 +5,19 @@ def is_even(input):
     uneven numbers to be False
     assert error if not a number (Integer)
     """
-    if (input%2)==0:
-        return True
-    else:  
-        return False
+    if not isinstance(input, int): 
+        raise TypeError('not an int')
+    
+    return (input%2)==0
 
 assert is_even.__doc__
 assert is_even(2)
 assert not is_even(1)
+
+has_thrown = False
+try:
+    is_even(0.0)
+except TypeError:
+    has_thrown = True
+assert has_thrown
 
