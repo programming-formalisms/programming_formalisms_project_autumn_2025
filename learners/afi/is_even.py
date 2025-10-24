679146273ab4a161abf8aa524bfd0eae4160bf23
afi3008 <annalenafischer3099@gmail.com>
2025-10-24 15:32:29 +0200

    is_even doc

---
+++ learners/afi/is_even.py
@@ -0,0 +1,10 @@
+def is_even():
+    """
+    function to determine if soemthing is even:
+    even numbers to be True
+    uneven numbers to be False
+    assert error if not a number (Integer)
+    """
+
+
+assert is_even.__doc__
