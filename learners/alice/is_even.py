74e8b40300aae4be9d0a557126c697f605df65b5
alicefurl <alice.furlotti@geo.uu.se>
2025-10-24 16:09:14 +0200

    added if statement to raise error for non-integer values

--- learners/alice/is_even.py
+++ learners/alice/is_even.py
@@ -3,6 +3,9 @@
 def is_even(number):
     """Defines if input number is even"""
     # Here I have defined is_even
+    if not isinstance(number, int):
+        raise TypeError("'number' must be of type int")
+    
     if number % 2 == 0:
         return True
     else:
@@ -10,3 +13,14 @@ def is_even(number):
 
 assert is_even.__doc__
 assert is_even(2) == True
+assert is_even(3) == False
+
+
+has_thrown = False
+try:
+    is_even(0.13)
+except TypeError:
+    has_thrown = True
+assert has_thrown
+
+
