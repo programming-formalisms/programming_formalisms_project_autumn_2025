911f72195cae595ef46b0b125f808f3804847422
alicefurl <alice.furlotti@geo.uu.se>
2025-10-24 14:40:20 +0200

    Added documentation after asserting it has documentation

--- learners/alice/is_zero.py
+++ learners/alice/is_zero.py
@@ -1,7 +1,11 @@
 def is_zero(var):
+    "This is the documentation I had not added before"
     if not isinstance(var, int):
         raise TypeError("'x' must be of type int")
     if var == 0:
         return True
     else:
         return False
+    
+
+assert is_zero.__doc__
