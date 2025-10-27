77c56d1b31e751eb77a530380bafdd8b6accf722
cibi-sundaram <cibisundaram.94@outlook.com>
2025-10-27 15:24:16 +0100

    Created & added documentation

---
+++ learners/ACL/pair_programming_ACL.py
@@ -0,0 +1,11 @@
+def get_digits(input):
+    """Gets the integer and splits into digits
+    """
+
+    
+
+    return [1]
+
+assert get_digits.__doc__
+assert get_digits(1) == 1
+assert get_digits(12) == [1,2]
