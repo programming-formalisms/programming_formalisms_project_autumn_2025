2966643c1f3064288f34e84d4670d63147ab1301
cibi-sundaram <cibisundaram.94@outlook.com>
2025-10-27 14:48:19 +0100

    assert 0 to empty

--- learners/cibi/function_int_to_romna.py
+++ learners/cibi/function_int_to_romna.py
@@ -1,6 +1,7 @@
-def int_to_roman():
+def int_to_roman(input):
     """Convert an integer to roman."""
-
+    return "I"
 
 assert int_to_roman.__doc__
 assert int_to_roman(1) == "I"
+assert int_to_roman(0) == ""
