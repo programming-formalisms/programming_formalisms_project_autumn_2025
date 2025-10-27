fec6373e4a3f1540ace5c5067a3ef714ba85a65c
alicefurl <alice.furlotti@geo.uu.se>
2025-10-27 15:30:51 +0100

    fixed 1 issue

--- learners/alice/sum_divisors.py
+++ learners/alice/sum_divisors.py
@@ -1,9 +1,10 @@
 def sum_divisors(x):
     """Returns the Sum of input divisors"""
     summation = 0
+    if x==1:
+            return 1
     for divisor in range(1, x):
         if x%divisor == 0:
-            print(divisor)
             summation += divisor
 
     return summation
@@ -12,3 +13,4 @@ def sum_divisors(x):
 assert sum_divisors.__doc__ 
 assert sum_divisors(1) == 1
 assert sum_divisors(4) == 3
+assert sum_divisors(0) == 0
