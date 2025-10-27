1d7102b96dc0e1be3b3d92759ec7e43a73dd7005
Carlos Gueto-Tettay <carlosgueto@gmail.com>
2025-10-27 10:29:38 +0100

    new text on choose_stations function

--- src/weather/choose_station.py
+++ src/weather/choose_station.py
@@ -1,5 +1,6 @@
 def choose_station():
     '''
-    The function needs to be able to choose a station from the data
+    The function needs to be able to choose a station from the data.
+    Some text explainig the function
     '''
     pass
