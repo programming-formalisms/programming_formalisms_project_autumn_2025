2162b39c827142c0efe7ace30e926775a2172f11
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-10-27 15:36:32 +0100

    scaffolding the function

--- learners/alice/sum_divisors.py
+++ learners/alice/sum_divisors.py
@@ -1,8 +1,8 @@
 def sum_divisors(x):
     """Returns the Sum of input divisors"""
     summation = 0
-    if x==1:
-            return 1
+    if x==1 or isinstance(x, float):
+            return 0
     for divisor in range(1, x):
         if x%divisor == 0:
             summation += divisor
@@ -11,6 +11,15 @@ def sum_divisors(x):
 
 
 assert sum_divisors.__doc__ 
-assert sum_divisors(1) == 1
+assert sum_divisors(1) == 0
 assert sum_divisors(4) == 3
 assert sum_divisors(0) == 0
+assert sum_divisors(-1) == 0
+assert sum_divisors(1.5) == 0
+has_thrown = False
+try:
+    sum_divisors("test")
+except:
+    has_thrown = True
+
+assert has_thrown
