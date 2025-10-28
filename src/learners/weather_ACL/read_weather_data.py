264e95591962679c97f4424051ddb13fe6536a75
SLAment <SLAment@users.noreply.github.com>
2025-10-28 10:55:33 +0100

    Function now evaluates if file is empty

--- src/learners/weather_ACL/read_weather_data.py
+++ src/learners/weather_ACL/read_weather_data.py
@@ -1,8 +1,22 @@
 import os
+
 def read_data(file_path):
     """Read the weather data from .dat file."""
     # Check if the file exists
-    return os.path.exists(file_path)
+    if not os.path.exists(file_path):
+        return False
+
+    # open the file in read mode
+    with open(file_path, 'r') as file_obj:
+        # read first character
+        first_char = file_obj.read(1)
+
+        if not first_char:
+            return False
+        else:
+            return True
+
 
 assert read_data.__doc__
-assert read_data('data/uppsala_tm_1722-2022.dat')
+assert read_data("data/uppsala_tm_1722-2022.dat")
+
