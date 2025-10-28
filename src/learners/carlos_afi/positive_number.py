c0cb5efdb0b52988f29b55fea5ad2477fb7891d8
afi3008 <annalenafischer3099@gmail.com>
2025-10-28 13:37:26 +0100

    fixed issues

--- src/learners/carlos_afi/positive_number.py
+++ src/learners/carlos_afi/positive_number.py
@@ -1,16 +1,18 @@
-"A class for positive number."
+"""A class for positive number."""
 
 class PositiveNumber:
+
+    """A class for positive number."""
+
     def __init__(self,number:int):
+        """Initialize."""
         assert self.is_positive(number)
         self._number = number
 
     def get_number(self):
+        """Get the number."""
         return self._number
-    
+
     def is_positive(self, number:int)->bool:
         """Check if number is positive."""
         return number > 0
-    
-
-
