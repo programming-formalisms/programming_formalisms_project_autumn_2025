d755eb9d9268c74d35df2d8e43f509903097b852
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-10-29 05:24:58 +0100

    The package does something

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -6,5 +6,6 @@ class TestAnalysis(unittest.TestCase):
 
     def test_analysis_creates_two_files(self):
         do_analysis()
-        # self.assertTrue(file_exists("figure.png"))
-        # self.assertTrue(file_exists("statistics_results.txt"))
+        import os.path
+        self.assertTrue(os.path.isfile("figure.png"))
+        self.assertTrue(os.path.isfile("statistics_results.txt"))
