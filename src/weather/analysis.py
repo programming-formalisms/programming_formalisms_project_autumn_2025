fa35efe3916160703942b0c636b3e320747e1ff2
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-10-29 07:47:26 +0100

    Fix style

--- src/weather/analysis.py
+++ src/weather/analysis.py
@@ -3,7 +3,7 @@ def do_analysis():
     figure_file = open("figure.png", "w")
     figure_file.write("Stub for an image")
     figure_file.close()
-    
+
     stats_file = open("statistics_results.txt", "w")
     stats_file.write("Stub for a statistics file")
     stats_file.close()
